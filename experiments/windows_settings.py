93ff193e04467d25ab8c404afc5d34628dd1aefe
Felix-vans <vanslegtenhorst.felix@gmail.com>
2026-01-08 15:59:16 +0100

    Update windows_settings.py

--- experiments/windows_settings.py
+++ experiments/windows_settings.py
@@ -1,6 +1,17 @@
+
 # still in devolpmemnt!
 
 import subprocess
+import winreg
+
+ADVANCED_PATH = r"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+
+SETTINGS = {
+    "ListviewAlphaSelect": 0,
+    "ListviewShadow": 0,
+    "TaskbarAnimations": 0,
+    "EnableAeroPeek": 0,
+}
 
 
 """
@@ -27,18 +38,44 @@ def gamemode():
 
 
 def ultimate_powerplan():
-    cmd = "powercfg /list"
-    output = subprocess.check_output(cmd, shell=True, text=True)
-    if output.find("e9a42b02-d5df-448d-aa00-03f14749eb61") == -1:
-        return False
+    cmd_1 = "powercfg /list"
+    cmd_2 = "powercfg /getactivescheme"
+    ultimate_powerplan_key = "e9a42b02-d5df-448d-aa00-03f14749eb61"
+    output = subprocess.check_output(cmd_1, shell=True, text=True)
+    if output.find(ultimate_powerplan_key) == -1:
+        exists = False
     else:
-        return True
+        exists = True
+    if exists:
+		output = subprocess.check_output(cmd_2, shell=True, text=True)
+		if output.find(ultimate_powerplan_key) == -1:
+			return 1, 0 #exists but not turned on
+		elif output.find(ultimate_powerplan_key):
+			return 1, 1 #exists and turned on
+	else:
+		return 0, 0 #doens't exist
 
 
-def performance_options():
-    ListViewAlphaSelect = subprocess.check_output("")
 
-    pass
+def read_registry_dword(path, name):
+    try:
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, path) as key:
+            value, regtype = winreg.QueryValueEx(key, name)
+            if regtype == winreg.REG_DWORD:
+                return int(value)
+    except FileNotFoundError:
+        return 0
+    except OSError:
+        return 0
+    return 0
+
+results = {}
+
+for setting in SETTINGS:
+    results[setting] = read_registry_dword(ADVANCED_PATH, setting)
+
+print(results)
+
 """
 HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
   ListviewAlphaSelect = 0
